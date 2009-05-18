55eafdb3e064534503fa21f7cb7ce4a5d01690b2
Sam Soffes <sam@samsoff.es>
2009-05-18 01:43:28 -0500

    Added Compass CSS framework

---
+++ config.rb
@@ -0,0 +1,7 @@
+# Require any additional compass plugins here.
+project_type = :stand_alone
+css_dir = "css"
+sass_dir = "sass"
+images_dir = "img"
+output_style = :compact
+http_images_path = "/img"
